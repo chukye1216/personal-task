ba89f41ead0ae8791e329be4c8f2f6cc78ad41fd
Robbie <robbie331@>
2024-07-25 16:11:15 +0900

    Level1-4반복문을 사용하여 반복의 종료를 알려주는 “exit” 문자열을 입력하기 전까지 무한으로 계산을 진행할 수 있도록 소스 코드를 수정합니다. 완료

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,59 +1,75 @@
 package calculator;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        int num1 = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        int num2 = sc.nextInt();
+        //while문 사용하여 계산기 반복
+        while (true) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
 
-        System.out.println("사칙연산 기호를 입력하세요: "); // +, -, *, /
-        char operator = sc.next().charAt(0);
-        //result 값 초기화
-        int result = 0;
-        //swich문 사용해 연산 기호 구분하여 출력
-        switch (operator) {
-            case '+':
-                result = num1 + num2;
-                System.out.println("결과: " + result);
-                break;
+            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+            int num1 = sc.nextInt();
 
-            case '-':
-                result = num1 - num2;
-                System.out.println("결과: " + result);
-                break;
+            System.out.print("두 번째 숫자를 입력하세요: ");
 
+            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+            int num2 = sc.nextInt();
 
-            case '*':
-                result = num1 * num2;
-                System.out.println("결과: " + result);
-                break;
+            System.out.println("사칙연산 기호를 입력하세요: "); // +, -, *, /
+            char operator = sc.next().charAt(0);
 
+            //result 값 초기화
+            int result = 0;
 
-            case '/':
-                if (num2 != 0) {
-                    result = num1 / num2;
+            //swich문 사용해 연산 기호 구분하여 출력
+            switch (operator) {
+                case '+':
+
+                    result = num1 + num2;
                     System.out.println("결과: " + result);
-                } else {
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                }
+                    break;
+
+
+                case '-':
+                    result = num1 - num2;
+                    System.out.println("결과: " + result);
+                    break;
+
+
+                case '*':
+                    result = num1 * num2;
+                    System.out.println("결과: " + result);
+                    break;
+
+
+                case '/':
+                    //if문 사용하여 num2에 0이 오는 경우 해당 연산오류 내용 출력
+                    if (num2 == 0) {
+                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    } else {
+                        result = num1 / num2;
+                        System.out.println("결과: " + result);
+                    }
+                    break;
+
+
+                default:
+                    System.out.println("올바른 사칙연산 기호를 입력해주세요");
+                    break;
+
+
+            }
+            sc.nextLine();
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String exit = sc.nextLine();
+            if (exit.equals("exit")) {
                 break;
-
-
-            default:
-                System.out.println("올바른 사칙연산 기호를 입력해주세요");
-                break;
-
-
+            }
 
         }
-
-
     }
 }
