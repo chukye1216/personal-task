f6650c14314f2b7fd2db2b0a7970c77f840e7de7
Robbie <robbie331@>
2024-07-29 14:06:32 +0900

    Level2-6 Calculator 인스턴스를 생성(new)할 때 생성자를 통해 연산 결과를 저장하고 있는 컬렉션 필드가 초기화 되도록 수정합니다. 완료

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,6 +5,12 @@ import java.util.ArrayList;
 
 public class Calculator {
     /**
+     * Calculator 인스턴스를 생성(new)할 때 생성자를 통해 연산 결과를 저장하고 있는 컬렉션 필드가 초기화 되도록 수정합니다.
+     */
+    public Calculator(){
+        this.saveArray = new ArrayList<>();
+    }
+    /**
      * App 클래스의 main 메서드에서 Calculator 클래스의 연산 결과를 저장하고 있는 컬렉션 필드에 직접 접근하지 못하도록 수정
      */
     private ArrayList<Integer> saveArray = new ArrayList<>();
@@ -81,8 +87,8 @@ public class Calculator {
      * 간접 접근을 통해 필드에 접근하여 수정할 수 있도록 구현합니다. (Setter 메서드)
      * @param
      */
-    public void setSaveArray(ArrayList<Integer> saveArray){
-        this.saveArray = new ArrayList<>();
+    public void setSaveArray(int result){
+        this.saveArray.add(result);
     }
 }
 
