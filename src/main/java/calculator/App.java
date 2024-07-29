b5f220ec024054569e80c78ddcfcdbd3a007af7a
Robbie <robbie331@>
2024-07-29 15:53:48 +0900

    Level2-7 Calculator 클래스에 반지름을 매개변수로 전달받아 원의 넓이를 계산하여 반환해주는 메서드를 구현합니다. 완료

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,4 +1,6 @@
 package calculator;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
@@ -9,46 +11,68 @@ public class App {
 
         //while문 사용하여 계산기 반복
         while (true) {
-            System.out.println("첫 번째 숫자를 입력하세요: ");
+            System.out.println("사칙연산/원의 넓이 중 어떤 것을 구하시겠습니까?(calc/circle)");
+            String Choice = sc.nextLine();
+            if (Choice.equals("calc")) {
+                System.out.println("첫 번째 숫자를 입력하세요: ");
 
-            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-            int num1 = sc.nextInt();
+                // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+                int num1 = sc.nextInt();
 
-            System.out.println("두 번째 숫자를 입력하세요: ");
+                System.out.println("두 번째 숫자를 입력하세요: ");
 
-            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-            int num2 = sc.nextInt();
+                // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+                int num2 = sc.nextInt();
 
-            System.out.println("사칙연산 기호를 입력하세요: "); // +, -, *, /
-            char operator = sc.next().charAt(0);
+                System.out.println("사칙연산 기호를 입력하세요: "); // +, -, *, /
+                char operator = sc.next().charAt(0);
 
-            //Calculator에서 연산 수행 후 저장
-            try {
-                int answer = calc.calculate(num1, num2, operator);
-            } catch (CalcException e) {
-                System.out.println(e.getMessage());
+                //Calculator에서 연산 수행 후 저장
+                try {
+                    int result = calc.calculate(num1, num2, operator);
+                } catch (CalcException e) {
+                    System.out.println(e.getMessage());
+                }
+
+
+                sc.nextLine();
+
+                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                String remove = sc.nextLine();
+
+                //remove를 받으면 가장 먼저 입력받은 연산결과 삭제
+                if (remove.equals("remove")) {
+                    calc.removeResult();
+                }
+
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                String inquiry = sc.nextLine();
+
+                //inquiry를 받으면 저장된 연산결과 조회
+                if (inquiry.equals("inquiry")) {
+                    calc.inquiryResults();
+                }
+
+            } else if (Choice.equals("circle")) {
+                System.out.println("원의 반지름을 입력해주세요 :");
+                double radius = sc.nextDouble();
+                double answer = calc.calculateCircleArea(radius);
+
+                sc.nextLine();
+
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (CircleInquiry 입력 시 조회)");
+                String CircleInquiry = sc.nextLine();
+
+                if (CircleInquiry.equals("CircleInquiry")) {
+                    calc.circleInquiry();
+                }
+
             }
 
-
-            sc.nextLine();
-
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            String remove = sc.nextLine();
-
-            //remove를 받으면 가장 먼저 입력받은 연산결과 삭제
-            if (remove.equals("remove")) {
-                calc.removeResult();
-            }
-
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            String inquiry = sc.nextLine();
-            //inquiry를 받으면 저장된 연산결과 조회
-            if (inquiry.equals("inquiry")) {
-                calc.inquiryResults();
-            }
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             String exit = sc.nextLine();
+
             //exit 입력시 프로그램 종료
             if (exit.equals("exit")) {
                 break;
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,16 +3,27 @@ package calculator;
 import java.util.ArrayList;
 
 public class Calculator {
+    // 한번 입력된 반지름 값은 변하지 않음 -> static
+    static double radius;
+    //파이 값은 절대로 변하지 않을 상수. -> final static
+    final static double PI = 3.14;
+    private ArrayList<Integer> saveArray = new ArrayList<>();
+    //원의 넓이를 저장할 새 리스트 생성
+    private ArrayList<Double> CircleArray = new ArrayList<>();
+
     /**
      * Calculator 인스턴스를 생성(new)할 때 생성자를 통해 연산 결과를 저장하고 있는 컬렉션 필드가 초기화 되도록 수정합니다.
      */
-    public Calculator(){
+    public Calculator() {
+        //생성자로 리스트 초기화
         this.saveArray = new ArrayList<>();
+        this.CircleArray = new ArrayList<>();
     }
+
     /**
      * App 클래스의 main 메서드에서 Calculator 클래스의 연산 결과를 저장하고 있는 컬렉션 필드에 직접 접근하지 못하도록 수정
      */
-    private ArrayList<Integer> saveArray = new ArrayList<>();
+
 
     public int calculate(int num1, int num2, char operator) throws CalcException {
         int result = 0;
@@ -51,7 +62,6 @@ public class Calculator {
                 throw new CalcException("잘못된 연산자를 입력했습니다.");
 
 
-
         }
         //리스트에 결과값 저장
         saveArray.add(result);
@@ -62,32 +72,58 @@ public class Calculator {
      * remove를 받으면 가장 먼저 입력받은 연산결과 삭제
      */
     public void removeResult() {
-            saveArray.remove(0);
+        saveArray.remove(0);
     }
 
     /**
      * inquiry를 입력받으면 저장된 연산 결과 조회
      */
     public void inquiryResults() {
-            for (int saveArray_res : saveArray) {
-                System.out.println(saveArray_res);
-            }
+        for (int saveArray_res : saveArray) {
+            System.out.println(saveArray_res);
+        }
     }
+    //원의 넓이를 구하는 메서드 생성
+    public double calculateCircleArea(double radius) {
+        double Cir = PI * radius * radius;
+        CircleArray.add(Cir);
+        System.out.println("원의 넓이 :" + Cir);
+        return Cir;
+
+
+    }
+    //저장된 원의 넓이 값들 바로 전체 조회
+    public void circleInquiry() {
+        for (double saveCircl : CircleArray) {
+            System.out.println(saveCircl);
+        }
+    }
+
 
     /**
      * 간접 접근을 통해 필드에 접근하여 가져올 수 있도록 구현합니다. (Getter 메서드)
+     *
      * @return
      */
-    public ArrayList<Integer> getSaveArray(){
+    public ArrayList<Integer> getSaveArray() {
         return saveArray;
     }
 
     /**
      * 간접 접근을 통해 필드에 접근하여 수정할 수 있도록 구현합니다. (Setter 메서드)
+     *
      * @param
      */
-    public void setSaveArray(int result){
-        this.saveArray.add(result);
+    public void setSaveArray(ArrayList<Integer> saveArray) {
+        this.saveArray = saveArray;
+    }
+
+    public ArrayList<Double> getCircleArray() {
+        return CircleArray;
+    }
+
+    public void setCircleArray(ArrayList<Double> CircleArray) {
+        this.CircleArray = CircleArray;
     }
 }
 
